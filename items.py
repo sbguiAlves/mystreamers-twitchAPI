e2bbfd5349d782723435babf6a1b7d6817dca561
Guilherme Alves <guilherme96.cdc@gmail.com>
2022-07-26 10:46:57 -0300

    style: class Items

--- items.py
+++ items.py
@@ -3,36 +3,38 @@ from collections import namedtuple
 from datetime import datetime, timedelta
 
 
-def customDataDecoder(dataDict):
-    return namedtuple('X', dataDict.keys())(*dataDict.values())
+class Items:
+    def customDataDecoder(dataDict):
+        return namedtuple('X', dataDict.keys())(*dataDict.values())
+
+    def verificar_status(status):
+        if status == 'live':
+            return 'ao vivo!'
+        return 'offline.'
+
+    def tempo_transmissao(data_str):
+        data_obj = datetime.strptime(data_str, "%Y-%m-%dT%H:%M:%SZ")
+        data_atual = datetime.now()
+        segundos = (data_atual - data_obj).seconds
+
+        if data_atual.date() == data_obj.date():
+            data_inicio = data_obj.strftime("%H:%M")
+        else:
+            data_inicio = data_obj.strftime("%H:%M (%d/%m)")
+
+        return f"Início: {data_inicio} <br> Transmitindo à: {str(timedelta(seconds=segundos))}"
+
+    # Main da classe
+    with open('streams.json', encoding="utf-8") as file:
+        json_file = file.read()
+        data_obj = json.loads(json_file, object_hook=customDataDecoder)
 
 
-def verificar_status(status):
-    if status == 'live':
-        return 'Estamos ao vivo! Vem'
-    return 'Estamos offline'
+    # for item in data_obj.data:
+    #     print(item.user_name + " \u2014 Status: " + verificar_status(item.type))
+    #     print("Título: " + item.title)
+    #     print("Jogo atual \u2014 " + item.game_name)
+    #     print(tempo_transmissao(item.started_at) + "\n")
 
-
-def tempo_transmissao(data_str):
-    data_obj = datetime.strptime(data_str, "%Y-%m-%dT%H:%M:%SZ")
-    data_inicio = data_obj.strftime("%d/%m, %H:%M:%S")
-
-    hora_atual = datetime.now()
-    segundos = (hora_atual - data_obj).seconds
-
-    return f"Começou às: {data_inicio} \u2014 Tempo de Transmissão: {str(timedelta(seconds=segundos))}"
-
-
-with open('streams.json', encoding="utf-8") as file:
-    json_file = file.read()
-    data_obj = json.loads(json_file, object_hook=customDataDecoder)
-
-
-for item in data_obj.data:
-    print(item.user_name + " \u2014 Status: " + verificar_status(item.type))
-    print("Título: " + item.title)
-    print("Jogo atual \u2014 " + item.game_name)
-    print(tempo_transmissao(item.started_at) + "\n")
-
-# print(type(data_obj.data[0].user_login))
-#print(tempo_transmissao(data_obj.data[0].started_at))
+    #print(type(data_obj))
+    # print(tempo_transmissao(data_obj.data[0].started_at))
